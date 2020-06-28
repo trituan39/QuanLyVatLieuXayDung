a6f0f00275e5989a00c385ee51cad535783b6346
117000121 <hoanggiahienlhu@gmail.com>
2020-06-28 22:10:05 +0700

    ThayDoiDatabase

---
+++ QuanLyVatLieuXayDung/QuanLyVatLieuXayDung/Class/Functions.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace QuanLyBanHang.Class
+{
+    class Functions
+    {
+        public static SqlConnection Con;    
+
+        public static void Connect()
+        {
+            Con = new SqlConnection();
+            Con.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=" + Application.StartupPath + @"\Quanlybanhang.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+            Con.Open();                  
+            
+            if (Con.State == ConnectionState.Open)
+                MessageBox.Show("Kết nối thành công");
+            else MessageBox.Show("Không thể kết nối với dữ liệu");
+
+        }
+        public static void Disconnect()
+        {
+            if (Con.State == ConnectionState.Open)
+            {
+                Con.Close();   	
+                Con.Dispose(); 	
+                Con = null;
+            }
+        }
+    }
